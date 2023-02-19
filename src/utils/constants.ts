31e45e540b58a2684c5fd8119de7924d9109b347
HadesHappy <happyhades123@gmail.com>
2023-02-20 00:41:36 +0900

    set time

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
-const ogStartTime = new Date("2023-02-19T15:00:00Z");
-const wlStartTime = new Date("2023-02-19T16:00:00Z");
-const publicStartTime = new Date("2023-02-19T16:30:00Z");
+const ogStartTime = new Date("2023-02-19T16:00:00Z");
+const wlStartTime = new Date("2023-02-19T16:30:00Z");
+const publicStartTime = new Date("2023-02-19T17:00:00Z");
 // const ogStartTime = new Date("2023-02-19T11:40:00Z");
 // const wlStartTime = new Date("2023-02-19T11:50:00Z");
 // const publicStartTime = new Date("2023-02-19T12:00:00Z");
