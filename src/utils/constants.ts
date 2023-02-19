193e31384a03d8c45078f5aee0dbf52c88e8e3f0
HadesHappy <happyhades123@gmail.com>
2023-02-20 00:26:44 +0900

    sd

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-const ogStartTime = new Date("2023-02-19T15:30:00Z");
+const ogStartTime = new Date("2023-02-19T15:00:00Z");
 const wlStartTime = new Date("2023-02-19T16:00:00Z");
 const publicStartTime = new Date("2023-02-19T16:30:00Z");
 // const ogStartTime = new Date("2023-02-19T11:40:00Z");
